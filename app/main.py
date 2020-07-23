2bc1c984b97b22d9d6ea81452e904368c92c5cd2
Sergey Misuk <sergey.misuk@gmail.com>
2020-07-24 01:41:11 +0300

    add db connection helper function

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from .database import SessionLocal, engine
+from . import models
 
 
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
