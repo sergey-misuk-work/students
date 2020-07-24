c6e746d07ba7041d2cd269c52e677cff6666302d
Sergey Misuk <sergey.misuk@gmail.com>
2020-07-24 12:28:07 +0300

    create a real route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
-from .database import SessionLocal, engine
-from . import models
+from fastapi import FastAPI, Depends
+from database import SessionLocal, engine
+import models, schemas
+from sqlalchemy.orm import Session
+from typing import List
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -15,6 +17,6 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/students", response_model=List[schemas.StudentRetrieveResponse])
+async def students(db: Session = Depends(get_db)):
+    return db.query(models.Student).all()
