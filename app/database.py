137f7d4bf4f4f68ce0c1bd614ca74e3e24a0160d
Sergey Misuk <sergey.misuk@gmail.com>
2020-07-24 13:48:55 +0300

    move db specific function to more suitable place

--- app/database.py
+++ app/database.py
@@ -6,3 +6,11 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db/postgres"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
