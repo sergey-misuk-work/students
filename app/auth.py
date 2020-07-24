46f7fd51a6620b0e9196a7a3b7e7d00bc185fffe
Sergey Misuk <sergey.misuk@gmail.com>
2020-07-24 13:51:30 +0300

    integrate jwt token flow

---
+++ app/auth.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from schemas import UserInternal, TokenData
+from sqlalchemy.orm import Session
+from main import get_db
+
+
+SECRET_KEY = '20265516bedc974d13cfa068f53cc41747a1c2a2de5425d5b29422f105e17b1f'
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_user(db: Session, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInternal(**user_dict)
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = get_user(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
--- app/main.py
+++ app/main.py
@@ -1,22 +1,35 @@
-from fastapi import FastAPI, Depends
-from database import SessionLocal, engine
-import models, schemas
+from fastapi import FastAPI, Depends, HTTPException, status
+from database import engine, get_db
+import models
+import schemas
 from sqlalchemy.orm import Session
 from typing import List
+from schemas import TokenRetrieve
+from fastapi.security import OAuth2PasswordRequestForm
+from auth import authenticate_user, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
+from datetime import timedelta
 
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 @app.get("/students", response_model=List[schemas.StudentRetrieveResponse])
 async def students(db: Session = Depends(get_db)):
     return db.query(models.Student).all()
+
+
+@app.post("/token", response_model=TokenRetrieve)
+async def create_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
