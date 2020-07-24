f5344df581e32e58b64b28e5c20dce512245f387
Sergey Misuk <sergey.misuk@gmail.com>
2020-07-24 12:28:40 +0300

    update test

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,31 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database import Base
+from main import app, get_db
 
-from main import app
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
+def test_retrieve_students():
+    response = client.get("/students")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == []
